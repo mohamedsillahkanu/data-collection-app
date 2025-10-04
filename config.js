4072c30eae25515297966ba64222a474256072a6
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2025-10-03 21:16:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const GITHUB_CONFIG = {
-    token: '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',
+    token: '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',
     owner: 'mohamedsillahkanu',
     repo: 'data-collection-app',
     branch: 'main'
