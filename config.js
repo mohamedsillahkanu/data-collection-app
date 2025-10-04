911140e0fbefb3c7a57645d87a53390837630e1e
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2025-10-03 20:53:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const GITHUB_CONFIG = {
     token: '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',
     owner: 'mohamedsillahkanu',
-    repo: 'health-data-submissions',
+    repo: 'data-collection-app',
     branch: 'main'
 };
